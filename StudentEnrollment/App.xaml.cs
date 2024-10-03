e7f7eb6bc22f2131b873aeb7a05f48b6071f9797
johnLeavell <johnleavell@gmail.com>
2024-10-03 09:25:48 -0500

    Add project files.

---
+++ StudentEnrollment/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentEnrollment
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ StudentEnrollment/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentEnrollment
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
