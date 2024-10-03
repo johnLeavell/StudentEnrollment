45c9bf7e16fc79ae19d664ffc582e1cbd5bacbea
johnLeavell <johnleavell@gmail.com>
2024-10-03 09:59:14 -0500

    added models

---
+++ StudentEnrollment/Models/Enrollment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEnrollment.Models
+{
+    public class Enrollment
+    {
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public string StudentName { get; set; }
+        public string CourseName { get; set; }
+    }
+}
